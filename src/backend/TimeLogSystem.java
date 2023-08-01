5e63d9916520644a5cf453286cd58703d6cfd17f
Diallo Souleymane <soul.diallo1998@gmail.com>
2023-08-01 11:23:51 -0400

    update de la classe TimeLogSystem

--- src/backend/TimeLogSystem.java
+++ src/backend/TimeLogSystem.java
@@ -11,15 +11,6 @@ public class TimeLogSystem {
         projects = new ArrayList<>();
         assignments = new ArrayList<>();
     }
-
-    public void addEmployee(Employe employe) {
-        employees.add(employe);
-    }
-
-    public void addProject(Projet project) {
-        projects.add(project);
-    }
-
     public List<Projet> getProjects() {
         return projects;
     }
@@ -27,9 +18,44 @@ public class TimeLogSystem {
     private List<Projet> projects;
     private List<Assignation> assignments;
 
+    public void addEmployee(Employe employe) {
+        employees.add(employe);
+    }
+    public void updateEmployee(Employe employe) {
+        // Vérifier si l'employé existe, puis mettre à jour ses informations
+        for (int i = 0; i < employees.size(); i++) {
+            if (employees.get(i).getID() == employe.getID()) {
+                employees.set(i, employe);
+                break;
+            }
+        }
+    }
+    public void deleteEmployee(int employeeID) {
+        // Vérifier si l'employé existe, puis le supprimer de la liste
+        employees.removeIf(employee -> employee.getID() == employeeID);
+    }
+
+    public void addProject(Projet project) {
+        projects.add(project);
+    }
+    public void updateProject(Projet projet) {
+        // Vérifier si le projet existe, puis mettre à jour ses informations
+        for (int i = 0; i < projects.size(); i++) {
+            if (projects.get(i).getID() == projet.getID()) {
+                projects.set(i, projet);
+                break;
+            }
+        }
+    }
+    public void deleteProject(int projectID) {
+        // Vérifier si le projet existe, puis le supprimer de la liste
+        projects.removeIf(project -> project.getID() == projectID);
+    }
+
     public void assignProject(Employe employe, Projet project, String discipline, Date startTime) {
         // Vérifier si l'employé existe et si le projet est valide, puis créer l'assignation
         Assignation assignment = new Assignation(employe, project, discipline, startTime, null);
         assignments.add(assignment);
     }
+
 }
