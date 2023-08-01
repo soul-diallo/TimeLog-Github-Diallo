b09c5fd150781ef9c5d93a6d620fe56d330f65cd
Diallo Souleymane <soul.diallo1998@gmail.com>
2023-08-01 10:50:26 -0400

    Ajout de la classe TimeLogSystem

--- src/backend/Assignation.java
+++ src/backend/Assignation.java
@@ -8,4 +8,52 @@ public class Assignation {
     private String discipline;
     private Date startTime;
     private Date endTime;
+
+    public Assignation(Employe employe, Projet projet, String discipline, Date startTime, Date endTime) {
+        this.employe = employe;
+        this.projet = projet;
+        this.discipline = discipline;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public Employe getEmploye() {
+        return employe;
+    }
+
+    public void setEmploye(Employe employe) {
+        this.employe = employe;
+    }
+
+    public Projet getProjet() {
+        return projet;
+    }
+
+    public void setProjet(Projet projet) {
+        this.projet = projet;
+    }
+
+    public String getDiscipline() {
+        return discipline;
+    }
+
+    public void setDiscipline(String discipline) {
+        this.discipline = discipline;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
 }
